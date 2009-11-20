91521ed1e8bfe5c1dccbf7826730345c9f3c0c45
Joon Lee <seouri@gmail.com>
2009-11-20 16:00:28 -0500

    clickable treemap

--- app/helpers/treemap_helper.rb
+++ app/helpers/treemap_helper.rb
@@ -4,7 +4,7 @@ module TreemapHelper
       content_tag(:ul, MeshTree.children.map {|c| content_tag(:li, link_to(c.subject.term, treemap_path(:id => c.id)), :id => "mesh_tree_id_#{c.id}")}.join("\n"))
     else
       children = content_tag(:ul, (tree.nil? ? MeshTree : tree).children.map {|c| content_tag(:li, link_to(c.subject.term, treemap_path(:id => c.id)), :id => "mesh_tree_id_#{c.id}")}.join("\n"))
-      current = content_tag(:ul, content_tag(:li, tree.subject.term + children, :id => "mesh_tree_id_#{tree.id}"))
+      current = content_tag(:ul, content_tag(:li, content_tag(:strong, tree.subject.term) + children, :id => "mesh_tree_id_#{tree.id}"))
       tree.ancestors.reverse.inject(current) {|html, c|  content_tag(:ul, content_tag(:li, link_to(c.subject.term, treemap_path(:id => c.id)) + "\n" + html, :id => "mesh_tree_id_#{c.id}"))}
     end
   end
@@ -12,15 +12,18 @@ module TreemapHelper
   def treemap(tree, year = "all")
     json = ""
     root = ""
+    tree_id = {}
     children = {}
-    (tree.nil? ? MeshTree : tree).children.select {|c| c.subject_stats.year(year)[0].nil? == false && c.subject_stats.year(year)[0].total_count > 0}.each {|c| children[c.subject.term] = c.subject_stats.year(year)[0].total_count}
+    (tree.nil? ? MeshTree : tree).children.select {|c| c.subject_stats.year(year)[0].nil? == false && c.subject_stats.year(year)[0].total_count > 0}.each {|c| children[c.subject.term] = c.subject_stats.year(year)[0].total_count; tree_id[c.subject.term.gsub(/[ ,]+/, '_')] = c.id}
     if tree.nil?
       json =  children.to_json
       root = "MeSH"
     else
+      tree_id[tree.subject.term.gsub(/[ ,]+/, '_')] = tree.id
       root = tree.subject.term
-      self_count = tree.subject_stats.year(year)[0].self_count
-      desc_count = tree.subject_stats.year(year)[0].descendant_count
+      ys = tree.subject_stats.year(year)[0]
+      self_count = ys.nil? ? 0 : ys.self_count
+      desc_count = ys.nil? ? 0 : ys.descendant_count
       if desc_count > 0
         children[tree.subject.term] = self_count if self_count > 0
         json = children.to_json
@@ -28,28 +31,34 @@ module TreemapHelper
         json = {tree.subject.term, self_count}.to_json
       end
     end
+
     mesh = "var mesh = " + json
     js = <<END
+<div id="treemap">
     <script type="text/javascript+protovis">
+      var tree_id = #{tree_id.to_json}
       #{mesh}
       var color = pv.Colors.category19().by(function(n) n.keys.slice(0, -1));
 
       var vis = new pv.Panel()
-        .width(600)
-        .height(600);
+        .width(800)
+        .height(400);
 
       vis.add(pv.Bar)
         .extend(pv.Layout.treemap(mesh).root("#{root}").round(true).inset(14, 4, 4, 4))
         .width(function(n) n.width - 2)
         .height(function(n) n.height - 2)
+        .cursor("pointer")
+        .event("click", function(n) top.location = "/treemap/" + tree_id[n.keys[1].replace(/[ ,]+/g, '_')])
         .fillStyle(function(n) color(n))
-        .title(function(n) n.keys.join(".") + ": " + n.data)
+        .title(function(n) n.keys[n.keys.length - 1].replace(/[ ,]+/g, '_') + ": " + (n.data || ""))
         .anchor("top").add(pv.Label)
         .textStyle(function() "rgba(0,0,0," + this.fillStyle().opacity + ")")
         .text(function(n) n.keys.length <= 3 ? n.keys[n.keys.length - 1] : "");
       vis.render();
 
     </script>
+</div>
 END
   end
 end
